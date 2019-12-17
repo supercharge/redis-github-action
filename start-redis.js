58ee66f6f590fdda80aad3e6ad1df32751312bda
Marcus Pöhls <marcus.poehls@gmail.com>
2019-12-17 15:33:26 +0100

    add deps

--- start-redis.js
+++ start-redis.js
@@ -2,6 +2,7 @@ const core = require('@actions/core')
 const { exec } = require('@actions/exec')
 
 async function  run (){
+  await exec(`npm install`)
   await exec(`docker run --name redis --publish 6379:6379 --detach redis:${core.getInput('redis-version')}`)
 }
 
