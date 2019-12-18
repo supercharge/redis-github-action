d845824559114285857af6e1cdfaf97654d73c58
Marcus Pöhls <marcus.poehls@gmail.com>
2019-12-18 10:57:59 +0100

    dos2unix

--- start-redis.sh
+++ start-redis.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-docker_run="docker run"
-docker_run="$docker_run --name redis --publish 6379:6379 --detach redis:$1"
-
-sh -c "$docker_run"
+docker run --name redis --publish 6379:6379 --detach redis:$1"
