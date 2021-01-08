347ff8a38d378e778bdb2213872db4816a76d78b
Marcus Pöhls <marcus.poehls@gmail.com>
2021-01-08 10:56:47 +0100

    check Redis version and fall back to latest

--- start-redis.sh
+++ start-redis.sh
@@ -2,4 +2,11 @@
 
 REDIS_VERSION=$1
 
+if [ -z "$REDIS_VERSION" ]; then
+  echo "Missing Redis version in the [redis-version] input. Received value: $REDIS_VERSION"
+  echo "Falling back to Redis version [latest]"
+  REDIS_VERSION='latest'
+fi
+
+echo "Starting single-node Redis instance"
 docker run --name redis --publish 6379:6379 --detach redis:$REDIS_VERSION
