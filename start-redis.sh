9c4929a4702a620bd5a3e590bd576ae1b91b8d74
Kien Pham <kien@codesee.io>
2023-07-25 15:36:40 -0700

    Add option to remove container when it exits

--- start-redis.sh
+++ start-redis.sh
@@ -4,6 +4,7 @@ REDIS_IMAGE=$1
 REDIS_VERSION=$2
 REDIS_PORT=$3
 REDIS_CONTAINER_NAME=$4
+REDIS_REMOVE_CONTAINER=$5
 
 if [ -z "$REDIS_VERSION" ]; then
   echo "Missing Redis version in the [redis-version] input. Received value: $REDIS_VERSION"
@@ -11,5 +12,11 @@ if [ -z "$REDIS_VERSION" ]; then
   REDIS_VERSION='latest'
 fi
 
-echo "Starting single-node Redis instance"
-docker run --name $REDIS_CONTAINER_NAME --publish $REDIS_PORT:6379 --detach $REDIS_IMAGE:$REDIS_VERSION
+DOCKER_RUN_ARGS="--name $REDIS_CONTAINER_NAME --publish $REDIS_PORT:6379 --detach $REDIS_IMAGE:$REDIS_VERSION"
+
+if [ "$REDIS_REMOVE_CONTAINER" == "true" ] ; then
+  DOCKER_RUN_ARGS+=" --rm"
+fi
+
+echo "Starting single-node Redis instance: $DOCKER_RUN_ARGS"
+docker run $DOCKER_RUN_ARGS
